cb82f3de6c1d6bfb2c9e813faaf2605255d34214
khoitruong9802 <khoitruong9802@gmail.com>
2023-11-07 21:30:32 +0700

    Commit 1

---
+++ client/client.py
@@ -0,0 +1,23 @@
+import socket
+
+# Client configuration
+server_ip = '127.0.0.1'  # Change to the server's IP address
+server_port = 12345      # Change to the server's port number
+buffer_size = 1024
+
+# Create a socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect to the server
+client_socket.connect((server_ip, server_port))
+
+# Send the file
+file_to_send = open('file_to_send.txt', 'rb')
+data = file_to_send.read(buffer_size)
+while data:
+    client_socket.send(data)
+    data = file_to_send.read(buffer_size)
+
+# Close the socket and the file
+client_socket.close()
+file_to_send.close()
---
+++ server/server.py
@@ -0,0 +1,33 @@
+import socket
+
+# Server configuration
+server_ip = '127.0.0.1'  # Change to your server's IP address
+server_port = 12345      # Change to your desired port number
+buffer_size = 1024
+
+# Create a socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to the address and port
+server_socket.bind((server_ip, server_port))
+
+# Listen for incoming connections
+server_socket.listen(1)
+print(f"Listening on {server_ip}:{server_port}")
+
+# Accept a client connection
+client_socket, client_address = server_socket.accept()
+print(f"Accepted connection from {client_address}")
+
+# Receive the file
+received_file = open('received_file.txt', 'wb')
+while True:
+    data = client_socket.recv(buffer_size)
+    if not data:
+        break
+    received_file.write(data)
+
+# Close the sockets and the file
+received_file.close()
+client_socket.close()
+server_socket.close()
